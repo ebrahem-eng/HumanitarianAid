7010c52574a9277f7742749b6b2078153c93bc10
Ebrahem Alwish <ebrahemk968@gmail.com>
2024-06-03 16:33:44 +0300

    Finish Store Keeper 1

--- app/Http/Controllers/Employe/StoreKeeperEmploye/StoreKeeperEmployeController.php
+++ app/Http/Controllers/Employe/StoreKeeperEmploye/StoreKeeperEmployeController.php
@@ -7,6 +7,7 @@ use App\Models\Aid;
 use App\Models\AidDistributionCampaigns;
 use App\Models\AidForAidDistributionCampaigns;
 use App\Models\AidRecieptCampaigns;
+use App\Models\AidRecivedFromAidRecivingCampaigns;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 
@@ -23,11 +24,10 @@ class StoreKeeperEmployeController extends Controller
 
         $storeKeeperEmployes = Employee::findOrfail($id);
 
-        return view('Employe.StoreKeeper.profile' , compact('storeKeeperEmployes'));
-
+        return view('Employe.StoreKeeper.profile', compact('storeKeeperEmployes'));
     }
 
-    public function storeKeeperEmployeProfileUpdate(Request $request , $id)
+    public function storeKeeperEmployeProfileUpdate(Request $request, $id)
     {
 
         $name = $request->input('name');
@@ -48,49 +48,44 @@ class StoreKeeperEmployeController extends Controller
             'address' => $address,
         ]);
 
-        return redirect()->back()->with('suceessMessage' , 'Profile Updated Successfully');
+        return redirect()->back()->with('suceessMessage', 'Profile Updated Successfully');
     }
 
     public function aidIndex()
     {
         $aids = Aid::all();
-        return view('Employe.StoreKeeper.Aid.index' , compact('aids'));
+        return view('Employe.StoreKeeper.Aid.index', compact('aids'));
     }
 
-    public function aidDelete(Request $request , $id)
+    public function aidDelete(Request $request, $id)
     {
 
         $aid = Aid::findOrfail($id);
         $quantityDeleted = $request->input('quantity');
         $originalQuantity = $aid->quantity;
 
-        if($quantityDeleted > $originalQuantity)
-        {
-            return redirect()->back()->with('errorMessage' , 'Error The Quantity Is Larger Than The Original');
-        }else if($quantityDeleted < $originalQuantity)
-        {
-             $newQuantity = $originalQuantity - $quantityDeleted;
-             $aid->update([
+        if ($quantityDeleted > $originalQuantity) {
+            return redirect()->back()->with('errorMessage', 'Error The Quantity Is Larger Than The Original');
+        } else if ($quantityDeleted < $originalQuantity) {
+            $newQuantity = $originalQuantity - $quantityDeleted;
+            $aid->update([
                 'quantity' => $newQuantity
-             ]);
-             return redirect()->back()->with('successMessage' , 'Aid Deleted Successfully');
+            ]);
+            return redirect()->back()->with('successMessage', 'Aid Deleted Successfully');
+        } else if ($quantityDeleted == $originalQuantity) {
+            $aid->delete();
+            return redirect()->back()->with('successMessage', 'Aid Deleted Successfully');
         }
-        else if($quantityDeleted == $originalQuantity)
-        {
-             $aid->delete();
-             return redirect()->back()->with('successMessage', 'Aid Deleted Successfully');
-        }
-
     }
 
     public function aidEdit($id)
     {
         $aid = Aid::findOrfail($id);
 
-        return view('Employe.StoreKeeper.Aid.edit' , compact('aid'));
+        return view('Employe.StoreKeeper.Aid.edit', compact('aid'));
     }
 
-    public function aidUpdate(Request $request , $id)
+    public function aidUpdate(Request $request, $id)
     {
         $aid = Aid::findOrfail($id);
         $name = $request->input('name');
@@ -110,20 +105,20 @@ class StoreKeeperEmployeController extends Controller
 
     public function aidDistributionCampaignsIndex()
     {
-        $aidDistributionCampaigns = AidDistributionCampaigns::where('status' , '0')->get();
-        return view('Employe.StoreKeeper.AidDistributionCampaigns.aidDistributionCampaignsIndex' , compact('aidDistributionCampaigns'));
+        $aidDistributionCampaigns = AidDistributionCampaigns::where('status', '0')->get();
+        return view('Employe.StoreKeeper.AidDistributionCampaigns.aidDistributionCampaignsIndex', compact('aidDistributionCampaigns'));
     }
 
     public function aidDistributionCampaignsAid($id)
     {
-       $aidDistributionCampaignsID = $id;
+        $aidDistributionCampaignsID = $id;
 
-       $aidForaidDistributionCampaigns = AidForAidDistributionCampaigns::where('AidDistributionID' , $aidDistributionCampaignsID)->get();
+        $aidForaidDistributionCampaigns = AidForAidDistributionCampaigns::where('AidDistributionID', $aidDistributionCampaignsID)->get();
 
-       return view('Employe.StoreKeeper.AidDistributionCampaigns.aidDistributionCampaignsAid' , compact('aidForaidDistributionCampaigns' , 'aidDistributionCampaignsID'));
+        return view('Employe.StoreKeeper.AidDistributionCampaigns.aidDistributionCampaignsAid', compact('aidForaidDistributionCampaigns', 'aidDistributionCampaignsID'));
     }
 
-    public function aidDistributionCampaignsAidAccept(Request $request , $id)
+    public function aidDistributionCampaignsAidAccept(Request $request, $id)
     {
 
         $aidForaidDistributionCampaignsID = $id;
@@ -139,11 +134,11 @@ class StoreKeeperEmployeController extends Controller
         $aidForaidDistributionCampaigns->update([
             'StatusReceiptStoreKeeper' => '1'
         ]);
-        
+
         return redirect()->back()->with('successMessage', 'Aid Accepted Successfully');
     }
 
-    public function aidDistributionCampaignsAidReject(Request $request , $id)
+    public function aidDistributionCampaignsAidReject(Request $request, $id)
     {
         $aidForaidDistributionCampaignsID = $id;
         $aidDistributionCampaignsID = $request->input('aidDistributionCampaignsID');
@@ -158,14 +153,61 @@ class StoreKeeperEmployeController extends Controller
         $aidForaidDistributionCampaigns->update([
             'StatusReceiptStoreKeeper' => '2'
         ]);
-        
+
         return redirect()->back()->with('successMessage', 'Aid Rejected Successfully');
-        
     }
 
     public function aidDistributionCampaignsHistory()
     {
-        $aidDistributionCampaigns = AidDistributionCampaigns::where('status' , '1')->get();
-        return view('Employe.StoreKeeper.AidDistributionCampaigns.aidDistributionCampaignsHistory' , compact('aidDistributionCampaigns'));
+        $aidDistributionCampaigns = AidDistributionCampaigns::where('status', '1')->get();
+        return view('Employe.StoreKeeper.AidDistributionCampaigns.aidDistributionCampaignsHistory', compact('aidDistributionCampaigns'));
+    }
+
+
+    public function aidReceivingIndex()
+    {
+        $AidRecieptCampaigns = AidRecieptCampaigns::all();
+        return view('Employe.StoreKeeper.AidReceiving.AidReceivingIndex', compact('AidRecieptCampaigns'));
+    }
+
+    public function aidReceivingAid($id)
+    {
+        $aidReceivingID = $id;
+        $aidForAidReceivings = AidRecivedFromAidRecivingCampaigns::where('AidReceiptID', $aidReceivingID)->get();
+
+        return view('Employe.StoreKeeper.AidReceiving.AidReceivingAid', compact('aidForAidReceivings', 'aidReceivingID'));
+    }
+
+    public function aidReceivingAidAccept(Request $request, $id)
+    {
+
+        $aidForAidReceivingID = $id;
+        $AidReceivingID = $request->input('aidReceivingID');
+
+        $AidReceiving = AidRecieptCampaigns::findOrfail($AidReceivingID);
+        $aidForAidReceivingCampaigns = AidRecivedFromAidRecivingCampaigns::findOrfail($aidForAidReceivingID);
+
+        $aidForAidReceivingCampaigns->update([
+            'statusAcceptStoreKeeper' => '1'
+        ]);
+
+        return redirect()->back()->with('successMessage', 'Aid Accepted Successfully');
+    }
+
+    public function aidReceivingAidReject(Request $request, $id)
+    {
+        $aidForAidReceivingID = $id;
+        $AidReceivingID = $request->input('aidReceivingID');
+        $reasonOfRefuse = $request->input('reasonOfRefuse');
+
+        $AidReceiving = AidRecieptCampaigns::findOrfail($AidReceivingID);
+        $aidForAidReceivingCampaigns = AidRecivedFromAidRecivingCampaigns::findOrfail($aidForAidReceivingID);
+
+        $aidForAidReceivingCampaigns->update([
+            'statusAcceptStoreKeeper' => '2',
+            'reasonOfRefuse' => $reasonOfRefuse,
+        ]);
+
+        return redirect()->back()->with('successMessage', 'Aid Rejected Successfully');
     }
 }
